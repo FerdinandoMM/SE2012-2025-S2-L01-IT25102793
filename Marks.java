949e9e4c2b828c5065f0dcf14d3f03fb235b5fd3
FerdinandoMM <it25102793@my.sliit.lk>
2026-07-22 12:19:14 +0530

    Started Activity 2

---
+++ Marks.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Marks {
+
+	private int studentsMax;
+	private String[] subjects = { "Mathematics", "Chemistry", "Physics" };
+	private int[][] marks;
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	void add ( int studentID ) {
+		
+	}
+
+	void update ( int studentID, int subjectID ) {
+
+	}
+
+	int getSubjectAverage ( int subjectID ) {
+
+	}
+
+	int getStudentAverage ( int studentID ) {
+
+	}
+
+	int getTotal ( int studentID ) {
+
+	}
+
+}
